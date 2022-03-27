197fde1c3edded9a49e7f9065ec218f43c31dda4
Jair-F <jair.fehlauer@gmail.com>
2022-03-27 20:17:05 +0300

    fixed some bugs...

--- Samba_Share/ServerResources/scripts/backup.sh
+++ Samba_Share/ServerResources/scripts/backup.sh
@@ -3,7 +3,7 @@
 # const vars
 BACKUP_FOLDER_PATH='../../Backups'
 COMPRESSION_TYPE='lz4'
-DATA_TO_BACKUP='../../Homes ../../Public-Shares'
+DATA_TO_BACKUP='../../Homes ../../PublicShares'
 BACKUPS_KEEP_DAILY=1
 BACKUPS_KEEP_WEEKLY=1
 BACKUPS_KEEP_MONTHLY=4
@@ -12,38 +12,43 @@ BACKUPS_KEEP_YEARLY=12
 
 # vars which change
 EXIT_CODE=1     # a temp variable used to get the result of commands
-TIME_DATE=`date "+%A_%d.%m.%Y_%H:%M:%S"`
-BACKUP_NAME="$HOSTNAME $TIME_DATE"  # depends on the date,time. outputs something like: "Saturday_26.03.2022_20:26:54"
-echo $BACKUP_NAME
+START_TIME_DATE=`date "+%A_%d.%m.%Y_%H:%M:%S"`
+BACKUP_NAME="$HOSTNAME $START_TIME_DATE"  # depends on the date,time. outputs something like: "Saturday_26.03.2022_20:26:54"
+# echo $BACKUP_NAME
 
 # check if a backup-repository exists - if not create one
 borg check --repository-only $BACKUP_FOLDER_PATH
 EXIT_CODE=$?
-if [[ $EXIT_CODE -neq 0 ]]
+if [[ $EXIT_CODE -ne 0 ]];
 then
     echo "IN $BACKUP_FOLDER_PATH does not exist a borg-repository - creating..."
-    borg init -e=none .
+    borg init -e=none $BACKUP_FOLDER_PATH
+else
+    echo "A borg-repository already exists..."
 fi
 
 # creating backup
-borg create --progress --compression $COMPRESSION_TYPE $BACKUP_FOLDER_PATH::$BACKUP_NAME $DATA_TO_BACKUP
+borg create -v --progress --compression "$COMPRESSION_TYPE" "$BACKUP_FOLDER_PATH::$BACKUP_NAME" "$DATA_TO_BACKUP"
 EXIT_CODE=$?
-if [[ $EXIT_CODE -neq 0]]
+if [[ $EXIT_CODE -ne 0 ]];
 then
-    echo "[ERROR] `date "+%A_%d.%m.%Y_%H:%M:%S"`: Error creating backup $BACKUP_NAME from $DATA_TO_BACKUP to $BACKUP_FOLDER_PATH"
+    TIME_DATE=`date "+%A_%d.%m.%Y_%H:%M:%S"`
+    echo $TIME_DATE
+    echo "[ERROR] $TIME_DATE: Error creating backup $BACKUP_NAME from $DATA_TO_BACKUP to $BACKUP_FOLDER_PATH"
 fi
 
 # removing old backups
-borg purne -v --list --keep-daily=$BACKUPS_KEEP_DAILY --keep-weekly=$BACKUPS_KEEP_WEEKLY --keep-monthly=$BACKUPS_KEEP_MONTHLY --keep-yearly=$BACKUPS_KEEP_YEARLY --prefix=$HOSTNAME
+borg prune -v --list --keep-daily=$BACKUPS_KEEP_DAILY --keep-weekly=$BACKUPS_KEEP_WEEKLY --keep-monthly=$BACKUPS_KEEP_MONTHLY --keep-yearly=$BACKUPS_KEEP_YEARLY --prefix=$HOSTNAME $BACKUP_FOLDER_PATH
 EXIT_CODE=$?
-if [[ $EXIT_CODE -neq 0]]
+if [[ $EXIT_CODE -ne 0 ]];
 then
     echo "[ERROR] `date "+%A_%d.%m.%Y_%H:%M:%S"`: Error deleting old backups in $BACKUP_FOLDER_PATH"
 fi
 
 # checking the made backup
-bor check .
-if [[ $EXIT_CODE -neq 0]]
+borg check $BACKUP_FOLDER_PATH
+EXIT_CODE=$?
+if [[ $EXIT_CODE -ne 0 ]];
 then
     echo "[ERROR] `date "+%A_%d.%m.%Y_%H:%M:%S"`: backup check in $BACKUP_FOLDER_PATH failed"
 fi

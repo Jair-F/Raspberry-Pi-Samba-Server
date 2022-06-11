31d4c29897fa98bb48872179681f338d62cc9e7f
Jair Fehlauer <jair.fehlauer@gmail.com>
2022-06-11 13:15:32 +0300

    path fix

--- Samba_Share/ServerResources/scripts/addUser.sh
+++ Samba_Share/ServerResources/scripts/addUser.sh
@@ -11,12 +11,11 @@ chown -R $USER:$USER /home/$USER
 chmod -R 770 /home/$USER
 
 # Samba home folder on Samba Data Device
-mkdir /media/Samba_Share_Device/Samba_Share/Homes/$USER
-chown -R $USER:$USER /media/Samba_Share_Device/Samba_Share/Homes/$USER
-chmod -R 770 /media/Samba_Share_Device/Samba_Share/Homes/$USER
+mkdir /media/Data/Samba_Share_Device/Samba_Share/Homes/$USER
+chown -R $USER:$USER /media/Data/Samba_Share_Device/Samba_Share/Homes/$USER
+chmod -R 770 /media/Data/Samba_Share_Device/Samba_Share/Homes/$USER
 
 echo $PASSWORD > /home/$USER/password
 
 cat /home/$USER/password /home/$USER/password | passwd $USER
 cat /home/$USER/password /home/$USER/password | smbpasswd -a $USER
-

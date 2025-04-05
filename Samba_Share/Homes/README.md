e57033f4e0db0d55607b45d5a7a0b22d42009c53
Jair-F <jair.fehlauer@gmail.com>
2025-04-05 13:42:57 +0000

    removed no longer needed scripts

--- Samba_Share/Homes/README.md
+++
@@ -1,3 +0,0 @@
-In This Folder the private User-Folders(the folders, which are not public - only the user has access to his folder) are stored here. Every user has a folder named accordingly to his username.
-
-The folder "pi" is as example user here
--- Samba_Share/ServerResources/scripts/createAllFamilyUsers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/media/Data/Samba_Share_Device/Samba_Share/ServerResources/scripts/addUser.sh jair
-/media/Data/Samba_Share_Device/Samba_Share/ServerResources/scripts/addUser.sh david
-/media/Data/Samba_Share_Device/Samba_Share/ServerResources/scripts/addUser.sh liat
-/media/Data/Samba_Share_Device/Samba_Share/ServerResources/scripts/addUser.sh susanne
-/media/Data/Samba_Share_Device/Samba_Share/ServerResources/scripts/addUser.sh jehudah

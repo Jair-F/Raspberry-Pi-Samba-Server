4e5db0f11ca4fee9340b5efd91c0b95fe82a2439
Jair-F <jair.fehlauer@gmail.com>
2025-04-05 14:22:00 +0000

    shell linter fixes

--- .devcontainer/setup_devcontainer.sh
+++ .devcontainer/setup_devcontainer.sh
@@ -2,4 +2,4 @@
 
 git config --global user.name Jair-F
 git config --global user.email jair.fehlauer@gmail.com
-git config --global core.autocrlf true
+#git config --global core.autocrlf true
--- scripts/addUsers.sh
+++ scripts/addUsers.sh
@@ -2,7 +2,7 @@
 
 SCRIPT=$(realpath "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
-echo $SCRIPT_PATH
+echo "$SCRIPT_PATH"
 
 # delete password for user
 # passwd -d user
@@ -17,26 +17,27 @@ then
 	exit 1
 fi
 
-if [ ! -f $SCRIPT_PATH/sambaUsers.back ]
+if [ ! -f "$SCRIPT_PATH/sambaUsers.back" ]
 then
 	echo "samba users Backup File doesnt exists - not creating users"
 	exit 0
 fi
 
 echo "adding login user jair"
-useradd -m -p '$6$rvUVdxrCv5S/Zknq$RQLd9FA.H/iq4gMVUIQANgPp93jOO9itv7gAecODzL/C9c5xodhhYMsITpfTCZvAlFraK94TAwmAyAXYwKjmh/' \
+useradd -m -p "\$6\$rvUVdxrCv5S/Zknq\$RQLd9FA.H/iq4gMVUIQANgPp93jOO9itv7gAecODzL/C9c5xodhhYMsITpfTCZvAlFraK94TAwmAyAXYwKjmh/" \
  	-s /bin/bash -U -G sudo jair
 
-for username in $(cat "$SCRIPT_PATH/sambaUsers.back" | awk -F: '{print $1}')
+#for username in $(awk -F: '{print $1}' < "$SCRIPT_PATH/sambaUsers.back")
+while read -r username < "$(awk -F: '{print $1}' < "$SCRIPT_PATH/sambaUsers.back")"
 do
-	if [ $username == "jair" ] ; # we add him extra later
+	if [ "$username" == "jair" ] ; # we add him extra later
 	then
 		echo "skipping user jair - adding as login user later"
 		continue
 	fi
 	echo "adding nonlogin user $username"
 	#adduser -q --no-create-home --system --shell /sbin/nologin --disabled-password --disabled-login $username
-	useradd --no-create-home --shell /sbin/nologin $username
+	useradd --no-create-home --shell /sbin/nologin "$username"
 done
 
 # restoring samba users

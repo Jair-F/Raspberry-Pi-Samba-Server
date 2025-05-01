129efa83316b488ff5156b9fc0c16dc89429953b
Jair-F <jair.fehlauer@gmail.com>
2025-05-01 16:03:22 +0100

    push of state

---
+++ server/start_servers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clear
+cp -r hompage/* /media/server/hompage/
+docker compose up
---
+++ server/start_tunnel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -i ~/.ssh/Jaja8373.nextcloud.pem Jaja8373.nextcloud@Jaja8373-24256.portmap.io -N -R 24256:localhost:24256

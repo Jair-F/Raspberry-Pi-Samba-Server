c99df3bfc868d296095d16f29bb174caa369f10b
Jair-F <jair.fehlauer@gmail.com>
2025-05-02 16:24:27 +0300

    update install deps

--- server/install_deps.sh
+++ server/install_deps.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo apt install dnsutils
+curl -sSL https://get.docker.com | sh
+sudo apt update
+sudo apt install -y docker.io
+
+sudo apt install -y dnsutils git
+
+
+sudo apt install -y vim

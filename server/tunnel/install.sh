aeb452569c43a05bbac74cfd950acd1e31d5dc04
Jair-F <jair.fehlauer@gmail.com>
2025-05-03 18:56:26 +0300

    added fstab + tunnel install script changes

--- server/tunnel/install.sh
+++ server/tunnel/install.sh
@@ -5,6 +5,10 @@ cp ./start_portmapio_tunnel /usr/bin
 
 chmod +x /usr/bin/start_portmapio_tunnel
 
+systemctl enable sshtunnel.service
+systemctl start sshtunnel.service
+systemctl status sshtunnel.service
+
+
 echo "pace the file: /root/.ssh/Jaja8373.nextcloud.pem"
 echo "enable the tunnel with systemctl enable sshtunnel"
-systemctl start sshtunnel.service

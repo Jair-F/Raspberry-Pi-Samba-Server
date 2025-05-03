739c42697d8f8cb1a2016917ef8a3223be749c73
Jair-F <jair.fehlauer@gmail.com>
2025-05-03 18:10:04 +0300

    push of state

--- server/tunnel/install.sh
+++ server/tunnel/install.sh
@@ -5,4 +5,6 @@ cp ./start_portmapio_tunnel /usr/bin
 
 chmod +x /usr/bin/start_portmapio_tunnel
 
+echo "pace the file: /root/.ssh/Jaja8373.nextcloud.pem"
 echo "enable the tunnel with systemctl enable sshtunnel"
+systemctl start sshtunnel.service
--- server/tunnel/uninstall.sh
+++ server/tunnel/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-systemctl stop sshtunnel
-systemctl disable sshtunnel
+systemctl stop sshtunnel.service
+systemctl disable sshtunnel.service
 
 rm /etc/systemd/system/sshtunnel.service
 rm /usr/bin/start_portmapio_tunnel

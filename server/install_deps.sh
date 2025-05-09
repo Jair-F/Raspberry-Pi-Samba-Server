2b318d8af001a22d7c0f754415855c3c4c2dbb98
Jair-F <jair.fehlauer@gmail.com>
2025-05-09 16:21:40 +0300

    added tailscale dep + fstab nofail mount

--- server/install_deps.sh
+++ server/install_deps.sh
@@ -2,9 +2,15 @@
 
 curl -sSL https://get.docker.com | sh
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y docker.io qemu-system
+sudo apt install -y docker.io #qemu-system
 
+# VPN
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo apt-get update
+sudo apt-get install tailscale
+
+
+# tools for user
 sudo apt install -y dnsutils git autossh
-
-
 sudo apt install -y vim tmux

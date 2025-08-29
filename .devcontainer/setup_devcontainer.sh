36835b7e9c5b9129560d03d460161fed54df2f16
Jair-F <jair.fehlauer@gmail.com>
2025-08-29 19:01:54 +0000

    fix default config not defaulting to a empty file

--- .devcontainer/setup_devcontainer.sh
+++ .devcontainer/setup_devcontainer.sh
@@ -3,3 +3,5 @@
 git config --global user.name Jair-F
 git config --global user.email jair.fehlauer@gmail.com
 git config --global core.eol lf # Forces LF line endings in the working directory for text files.
+
+/bin/bash scripts/createFolderTree.sh "default_config"
